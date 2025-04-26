a23681c9c9ebdaba434f4787b8defc8a80632049
Pouya <pouya.esmaeili.g@gmail.com>
2025-04-27 03:25:38 +0330

    Deploy on cloudflare

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CfAiusuQ.mjs';
-import { manifest } from './manifest_C03PqEGj.mjs';
+import { manifest } from './manifest_DKdUJsS4.mjs';
 
 const serverIslandMap = new Map();;
 
