df67498091d214174c55f190faca814e710803cf
Pouya <pouya.esmaeili.g@gmail.com>
2025-05-06 01:37:50 +0330

    Add llm-inference-configuration.astro

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,17 +1,19 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CfAiusuQ.mjs';
-import { manifest } from './manifest_u0pEW9re.mjs';
+import { manifest } from './manifest_CoJSNu91.mjs';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/humanoid.astro.mjs');
-const _page1 = () => import('./pages/lm-studio.astro.mjs');
-const _page2 = () => import('./pages/index.astro.mjs');
+const _page1 = () => import('./pages/llm-inference-configuration.astro.mjs');
+const _page2 = () => import('./pages/lm-studio.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["src/pages/humanoid.astro", _page0],
-    ["src/pages/lm-studio.astro", _page1],
-    ["src/pages/index.astro", _page2]
+    ["src/pages/llm-inference-configuration.astro", _page1],
+    ["src/pages/lm-studio.astro", _page2],
+    ["src/pages/index.astro", _page3]
 ]);
 
 const _manifest = Object.assign(manifest, {
