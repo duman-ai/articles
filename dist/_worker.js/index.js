f71c64c3e94c2df72d0091aeb7535eafbae4c1d3
Pouya <pouya.esmaeili.g@gmail.com>
2025-05-11 19:20:30 +0330

    Update image size in prompt page.

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CfAiusuQ.mjs';
-import { manifest } from './manifest_CVYJi0l7.mjs';
+import { manifest } from './manifest_DPneA6V9.mjs';
 
 const serverIslandMap = new Map();;
 
