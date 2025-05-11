506307fe00fe2da6c0b4dcf6c77e6816d660283e
Pouya <pouya.esmaeili.g@gmail.com>
2025-05-11 19:10:34 +0330

    Add prompt and its techniques page.

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,19 +1,21 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CfAiusuQ.mjs';
-import { manifest } from './manifest_CIQEi39k.mjs';
+import { manifest } from './manifest_CVYJi0l7.mjs';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/humanoid.astro.mjs');
 const _page1 = () => import('./pages/llm-inference-configuration.astro.mjs');
 const _page2 = () => import('./pages/lm-studio.astro.mjs');
-const _page3 = () => import('./pages/index.astro.mjs');
+const _page3 = () => import('./pages/prompt.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["src/pages/humanoid.astro", _page0],
     ["src/pages/llm-inference-configuration.astro", _page1],
     ["src/pages/lm-studio.astro", _page2],
-    ["src/pages/index.astro", _page3]
+    ["src/pages/prompt.astro", _page3],
+    ["src/pages/index.astro", _page4]
 ]);
 
 const _manifest = Object.assign(manifest, {
